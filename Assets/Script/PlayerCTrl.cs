7f8b2cfde01b8e9280e034fa54bdaf76f70c468b
Kendo <kendosang_1994@hotmail.com>
2016-04-25 15:52:51 +0700

    First Commit

---
+++ Assets/Script/PlayerCTrl.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCTrl : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public int speed;
+
+	//Bomb
+	public static bool BombActivate;
+	public static int amountBomb;
+
+	//Item2
+	public static bool Item2Activate;
+	public static int amountI2;
+
+	public static int itemNow;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		speed = 10;
+		BombActivate = false;
+		amountBomb = 0;
+		itemNow = 0;
+	}
+
+	void Update () {
+
+		//PlayerCtrl
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			rb2d.velocity = new Vector2 (-speed,0);
+			transform.eulerAngles = new Vector2 (0, 180);
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			rb2d.velocity = new Vector2 (speed,0);
+			transform.eulerAngles = new Vector2 (0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftShift)) 
+		{
+			speed += 10;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) 
+		{
+			speed -= 10;
+		}
+
+		//Bomb Activate
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			if (amountBomb > 0) 
+			{
+				BombActivate = true;
+				Item2Activate = false;
+				itemNow = 1;
+			}
+		}
+		//Item2 Activate
+		if (Input.GetKeyDown (KeyCode.Alpha2)) 
+		{
+			if (amountI2 > 0) 
+			{
+				Item2Activate = true;
+				BombActivate = false;
+				itemNow = 2;
+			}
+		}
+
+		//Use Item
+		if (Input.GetKeyDown(KeyCode.E)) 
+		{
+			//Bomb
+			if (BombActivate = true && itemNow == 1) 
+			{
+				amountBomb -= 1;
+			}
+			//Item2
+			if (Item2Activate = true && itemNow == 2) 
+			{
+				amountI2 -= 1;
+			}
+		}
+		//Deactivate
+
+		//Bomb
+		if (amountBomb <= 0) 
+		{
+			BombActivate = false;
+			amountBomb = 0;
+		}
+		//Item2
+		if (amountI2 <= 0) 
+		{
+			Item2Activate = false;
+			amountI2 = 0;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "Bomb") {
+			Destroy (col.gameObject);
+			amountBomb += 1;
+		}
+		if (col.tag == "Item2") {
+			Destroy (col.gameObject);
+			amountI2 += 1;
+		}
+	}
+}
---
+++ Assets/Script/UITest.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITest : MonoBehaviour {
+	//itemText
+	public Text Item1Text;
+	public Text Item2Text;
+
+	//itemNow Text
+	public Text ItemNowText;
+	private int itemNow;
+
+	//item1
+	private bool item1_activate;
+	private int amountItem1;
+
+	//item2
+	private bool item2_activate;
+	private int amountItem2;
+
+	void Start ()
+	{
+		ItemNowText.text = "Empty";
+		Item1Text.text = "Bomb = 0";
+		Item2Text.text = "Item2 = 0";
+	}
+
+	void Update ()
+	{
+		//Item Text
+
+		//Bomb
+		item1_activate = PlayerCTrl.BombActivate;
+		if ((Input.GetKeyDown(KeyCode.Alpha1))) 
+		{
+			ItemNowText.text = "Bomb";
+		}
+		//Item2
+		item2_activate = PlayerCTrl.Item2Activate;
+		if (Input.GetKeyDown (KeyCode.Alpha2)) 
+		{
+			ItemNowText.text = "Item2";
+		}
+
+		//Item Amout Text
+
+		//Bomb
+		amountItem1 = PlayerCTrl.amountBomb;
+		Item1Text.text = "Bomb = " + amountItem1;
+
+		//Item2
+		amountItem2 = PlayerCTrl.amountI2;
+		Item2Text.text = "Item2 = " + amountItem2;
+	}
+}
